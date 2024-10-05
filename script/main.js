35bb633986b0abf17bcce4fde0a77863bb2bb4c6
if <55980773+Yifeeeeei@users.noreply.github.com>
2024-10-04 22:49:22 -0700

    sub menu

--- script/main.js
+++ script/main.js
@@ -1,3 +1,43 @@
+const configs = {
+    guide: {
+        name: "新人指南",
+        id: "guide",
+
+        function: function () {
+            toggleSubMenu("guide-submenu"); // Toggle sub-items for this menu
+        },
+        subItems: {
+            deckBuild: {
+                name: "卡组构筑",
+                function: function () {
+                    jumpToOther("https://yifeeeeei.github.io/SorceryComposer/");
+                },
+            },
+        },
+    },
+    story: {
+        name: "背景故事",
+        id: "story",
+        function: function () {
+            toggleSubMenu("story-submenu"); // Toggle sub-items for this menu
+        },
+        subItems: {
+            mermaid: {
+                name: "人鱼的故事",
+                function: function () {
+                    jumpHere("htmls/Mermaid.html");
+                },
+            },
+            Fire: {
+                name: "烈火之门派",
+                function: function () {
+                    jumpHere("htmls/Fires.html");
+                },
+            },
+        },
+    },
+};
+
 var baseDiv;
 var frameDiv;
 function showBase() {
@@ -20,9 +60,10 @@ function init() {
 }
 
 function loadFrame(path) {
-    var frame = document.getElementById("frame");
+    var frame = document.getElementById("iframe");
     frame.src = path;
     showFrame();
+    console.log("load frame: " + path + frame.src);
 }
 
 function jumpToOther(path) {
@@ -30,37 +71,53 @@ function jumpToOther(path) {
     window.open(path, "_blank");
 }
 
+function jumpHere(path) {
+    // open path in current tab
+    window.location.href = path;
+}
+
 function backToBase() {
     showBase();
 }
 
 function prepareMenuItems() {
-    // id: {name: function:}
-    configs = {
-        "build-your-deck": {
-            name: "构筑卡组",
-            function: function () {
-                jumpToOther("https://yifeeeeei.github.io/SorceryComposer/");
-            },
-        },
-        story: {
-            name: "故事:巴特尔",
-            function: function () {
-                loadFrame("1.html");
-            },
-        },
-    };
+    // id: {name: function:, subItems: {}}
 
-    // add to base menu
-    var baseMenu = document.getElementById("base-menu");
-    for (var key in configs) {
-        var item = document.createElement("ul");
+    const baseMenu = document.getElementById("base-menu");
+
+    // Create menu items
+    for (const key in configs) {
+        const item = document.createElement("ul");
         item.classList.add("menu-item");
         item.innerHTML = configs[key].name;
         item.onclick = configs[key].function;
+        item.id = configs[key].id;
         baseMenu.appendChild(item);
-        console.log(item);
+
+        // If there are sub-items, create a submenu
+        if (configs[key].subItems) {
+            const subMenu = document.createElement("ul");
+            subMenu.id = `${key}-submenu`;
+            subMenu.classList.add("submenu");
+
+            for (const subKey in configs[key].subItems) {
+                const subItem = document.createElement("li");
+                subItem.classList.add("menu-subitem");
+                subItem.innerHTML = configs[key].subItems[subKey].name;
+                subItem.onclick = configs[key].subItems[subKey].function;
+                subMenu.appendChild(subItem);
+            }
+            item.appendChild(subMenu);
+        }
     }
 }
 
+function toggleSubMenu(subMenuId) {
+    const subMenu = document.getElementById(subMenuId);
+    if (subMenu.classList.contains("show")) {
+        subMenu.classList.remove("show"); // Hide submenu
+    } else {
+        subMenu.classList.add("show"); // Show submenu
+    }
+}
 init();
